463bea9a66edb11f3ea2c5973972b5eb7384ad8b
sonam1331 <94792483+sonam1331@users.noreply.github.com>
2022-09-30 13:39:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,8 @@
-# module "ec2-ins" {
-#     source = "../ec2"
-    #name = "sonam-terraform"
-    #for_each = var.ec2_loop
-    # name  = var.ins_name
-    #name = each.key
-#     bucket-name2 = "2nd buck"
-}
-# module "s3-bucket" {
-#     source = "../s3"
-#     bucket-name = "My-first-buc"
-#     acl = "private"
-# }
-# # module "vpc" {
-# #     source = "../vpc"
-# #     cidr = ""
-# # }
+module "ec2-ins" {
+    source = "../ec2"
+    name = "sonam-terraform"
+    for_each = var.ec2_loop
+    name  = var.ins_name
+    name = each.key
 
-module "iam-user-mod"{
-    source = "../IAM"
-    
-}
-module "iam-group-mod"{
-    source = "../IAM"
-}
-module "iam-user-group-membership"{
-    depends_on = [
-        module.iam-user-mod
-    ]
 }
